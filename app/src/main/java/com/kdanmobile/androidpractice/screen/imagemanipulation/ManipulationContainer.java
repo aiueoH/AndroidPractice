2503c4ff3db2fbe6883d8fcabb247f6237be0274
Wayne <wayne.huang@kdanmobile.com>
2016-04-28 14:49:42 +0800

    完成縮放物件(view)功能
    可在 Image Manipulation 中操作縮放物件。

--- app/src/main/java/com/kdanmobile/androidpractice/screen/imagemanipulation/ManipulationContainer.java
+++ app/src/main/java/com/kdanmobile/androidpractice/screen/imagemanipulation/ManipulationContainer.java
@@ -3,6 +3,7 @@ package com.kdanmobile.androidpractice.screen.imagemanipulation;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -12,13 +13,16 @@ public class ManipulationContainer extends RelativeLayout {
     private View selectedItem = null;
     private float downX, downY;
     private int itemOriginalLeftMargin, itemOriginalTopMargin;
+    private ScaleGestureDetector scaleGestureDetector;
 
     public ManipulationContainer(Context context, AttributeSet attrs) {
         super(context, attrs);
+        scaleGestureDetector = new ScaleGestureDetector(getContext(), new OnScaleGestureListener());
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 downX = event.getX();
@@ -59,5 +63,28 @@ public class ManipulationContainer extends RelativeLayout {
                 return false;
             }
         });
+    }
+
+    private class OnScaleGestureListener implements ScaleGestureDetector.OnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float factor = detector.getScaleFactor();
+            for (int i = 0; i < getChildCount(); i++) {
+                View item = getChildAt(i);
+                item.setScaleX(item.getScaleX() * factor);
+                item.setScaleY(item.getScaleY() * factor);
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector detector) {
+
+        }
     }
 }
